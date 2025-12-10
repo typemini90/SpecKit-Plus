492190113afb7977fa72cef698f11c16578f1134
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-12-10 14:04:24 +0500

    fixed baseurl

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://Panaversity.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/dockathon/',
+  baseUrl: '/SpecKit-Plus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
